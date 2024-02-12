70432e6d56251bd5018331e8203dc1d6be997460
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2024-02-12 22:54:00 +0200

    ad

--- src/pages/PasswordResetSendCode.jsx
+++ src/pages/PasswordResetSendCode.jsx
@@ -4,6 +4,9 @@ const PasswordResetSendCode = () => {
   return (
     <div className="pwd-reset-code">
         <h1>send pwd reset code</h1>
+        <input type="text" placeholder='email'/>
+        <button>send email</button>
+        <button>resend email</button>
     </div>
   )
 }
